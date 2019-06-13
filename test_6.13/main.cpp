db59cbd0ce48c58a2401eceb66eb20c4bdc80cd0
袁袁媛 <2977759369@qq.com>
2019-06-13 11:42:31 +0800

    Initial Commit

---
+++ test_6.13/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test_6.13
+//
+//  Created by s20181106278 on 2019/6/13.
+//  Copyright © 2019 s20181106278. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
